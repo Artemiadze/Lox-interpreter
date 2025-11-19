50a5fb5d38f2fc0925e93f49cc6541ad0945ae83
Artyom Vlasov <t0mi.v@yandex.ru>
2025-11-19 18:56:13 +0300

    Implemented lexing for all characters

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,58 @@
 use std::env;
 use std::fs;
-use std::io::{self, Write};
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+use miette::{WrapErr, IntoDiagnostic};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        writeln!(io::stderr(), "Usage: {} tokenize <filename>", args[0]).unwrap();
-        return;
-    }
+use codecrafters_interpreter::*;
 
-    let command = &args[1];
-    let filename = &args[2];
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
 
-    match command.as_str() {
-        "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Tokenize { filename: PathBuf },
+}
 
-            let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
-                writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
-                String::new()
-            });
+fn main() -> miette::Result<()>{
+    let args = Args::parse();
 
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, replace this line when implementing the scanner
+    match args.command {
+        Commands::Tokenize{ filename } => {
+            let mut any_cc_err = false;
+            let file_contents = fs::read_to_string(&filename)
+            .into_diagnostic()
+            .wrap_err_with(|| format!("reading file {} failed", filename.display()))?;
+
+            for token in Lexer::new(&file_contents) {
+                let token = match token {
+                    Ok(t) => t,
+                    Err(e) => {
+                        eprintln!("{e:?}");
+                        if let Some(unrecognized) = e.downcast_ref::<SingleTokenError>() {
+                            any_cc_err = true;
+                            eprintln!(
+                                "[line {}] Error: Unexpected character: {}",
+                                unrecognized.line(),
+                                unrecognized.token
+                            );
+                        } else if let Some(unterminated) =
+                            e.downcast_ref::<StringTerminationError>()
+                        {
+                            any_cc_err = true;
+                            eprintln!("[line {}] Error: Unterminated string.", unterminated.line());
+                        }
+                        continue;
+                    }
+                };
+                println!("{token}");
             }
-        }
-        _ => {
-            writeln!(io::stderr(), "Unknown command: {}", command).unwrap();
-            return;
+            println!("EOF  null");
         }
     }
+    Ok(())
 }
