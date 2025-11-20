d50df9344385e7fd0c146bd8383e856399beb579
Artyom Vlasov <t0mi.v@yandex.ru>
2025-11-20 13:43:35 +0300

    implemented parsing expressions and statements

--- src/parse.rs
+++ src/parse.rs
@@ -27,40 +27,96 @@ impl<'de> Parser<'de> {
     }
 
     pub fn parse(mut self) -> Result<TokenTree<'de>, Error> {
-        self.parse_within(0)
+        self.parse_within(None, 0)
     }
 
     pub fn parse_within(&mut self, looking_for: Option<(Operator, usize)>, min_bp: u8) -> Result<Option,TokenTree<'de>>, Error> {
-        let looking_for = 
+        let looking_for_msg = || {
+            if let Some((op, argi)) = looking_for {
+                format!("looking_for argument #{argi} for operator {op:?}")
+            } else {
+                "looking_for a statement".to_string()
+            }
+        }
         let lhs = match self.lexer,next() {
             Some(Ok(token)) => token,
             None => return Ok(Tokentree::Atom(Atom::Nil)),
             Some(Err(e)) => {
-                let msg = if let Some((op, argi)) = looking_for {
-                    format!("looking_for argument #{argi} of operator {op:?}")
-                } else {
-                    "looking_for a statement".to_string()
-                }
+                let msg = looking_for();
                 return Err(e).wrap_err(msg)
             }
         };
 
         let mut lhs = match lhs {
+            // atoms
             Token {
                 kind: TokenKind::String,
                 origin,
             } => TokenTree::Atom(Atom::String(Token::unescape(origin))),
-            Token::Atom(it) => S::Atom(it),
-            Token::Op('(') => {
-                let lhs = self.parse_within(0);
-                assert_eq!(lexer.next(), Token::Op(')'));
-                lhs.wrap_err("group")?
+            Token {
+                kind: TokenKind::Number(n),
+                ..
+            } => TokenTree::Atom(Atom::Number(n)),
+            Token {
+                kind: TokenKind::True,
+                ..
+            } => TokenTree::Atom(Atom::Bool(true)),
+            Token {
+                kind: TokenKind::False,
+                ..
+            } => TokenTree::Atom(Atom::Bool(false)),
+            Token {
+                kind: TokenKind::Ident,
+                origin,
+            } => TokenTree:: Atom(Atom::Ident(origin)),
+
+            // Groups
+            Token {
+                kind: TokenKind::LeftParen | TokenKind::LeftBrace,
+                ..
+            } => {
+                let lhs = self.parse_within(looking_for, 0);
+                let terminator = match lhs.kind {
+                    TokenKind::LeftParen => TokenKind::RightParen,
+                    TokenKind::LeftBrace => TokenKind::RightBrace,
+                    - => unreachable!("by the outer match arm pattern"),
+                };
+                let lhs = self.parse_within(looking_for, 0).wrap_err("group")?;
+                match self.lexer.next(){
+                    Some(token) if token == terminator => {},
+                    Some(token) => {
+                        return Err(miette::miette!(
+                            labels = vec![
+                                LabeledSpan::at(self.byte - literal.len()..self.byte, "this numeric literal"),
+                            ],
+                            "{e}",
+                        }.with_source_code(self.whole.to_string())).wrap_err(looking_for_msg());
+                    }
+                    None => {
+                        return Err(Eof).wrap_err(looking_for_msg());
+                    }
+                }
+                assert_eq!(self.lexer.next().kind, terminator);
+                lhs
             }
-            Token::Op(op) => {
+
+            //prefix expressions
+            Token {
+                kind: TokenKind::Bang | TokenKind::Print | TokenKind::Minus,
+                ..
+            } =>  {
+                let op = match lhs.kind {
+                    TokenKind::Bang => Operator::Bang,
+                    TokenKind::Print => Operator::Print,
+                    TokenKind::Minus => Operator::Minus,
+                    - => unreachable!("by the outer match arm pattern"),
+                };
                 let ((), r_bp) = prefix_binding_power(op);
-                let rhs = self.parse_within(r_bp).wrap_err("parse RHS")?;
-                S::Cons(op, vec![rhs])
-            }
+                let rhs = self
+                    .parse_within(Some((op, 0)), r_bp)
+                    .wrap_err("parse RHS")?;
+                TokenTree::Cons(op, vec![rhs])
+            }  
             t => panic!("bad token: {:?}", t),
         };
 
@@ -179,9 +235,9 @@ impl fmt::Display for TokenTree<'_> {
     }
 }
 
-fn prefix_binding_power(op: char) -> ((), u8) {
+fn prefix_binding_power(op: Operator) -> ((), u8) {
     match op {
-        '+' | '-' => ((), 9),
+        Operator::Bang | Operator::Print | Operator::Minus => ((), 9),
         _ => panic!("bad op: {:?}", op),
     }
 }
